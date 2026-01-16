9b392ffdb2af255e1c6197454e0cb52b6cd34bbc
Stiller <ruupe.dev@gmail.com>
2026-01-16 18:10:00 +0200

    Tasks done

---
+++ week1/src/main/java/backend/week1/web/Week1Controller.java
@@ -0,0 +1,26 @@
+package backend.week1.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@ResponseBody
+public class Week1Controller {
+    @GetMapping("/index")
+    public String getIndex() {
+        return "This is the main page";
+    }
+
+    @GetMapping("/contact")
+    public String getContact() {
+        return "This is the contact page";
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam String location, String name) {
+        return "Welcome to the " + location + " " + name;
+    }
+
+}
