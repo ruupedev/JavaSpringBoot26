01573cd48d255eeeaae29ce3c373277cc9c90e0e
Stiller <ruupe.dev@gmail.com>
2026-02-01 20:11:12 +0200

    Added delete and add book ability

--- bookstore/src/main/java/bookstore/bookstore/web/BookController.java
+++ bookstore/src/main/java/bookstore/bookstore/web/BookController.java
@@ -1,9 +1,15 @@
 package bookstore.bookstore.web;
 
+import bookstore.bookstore.domain.Book;
 import bookstore.bookstore.domain.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BookController {
@@ -23,6 +29,24 @@ public class BookController {
     public String getBooklist(Model model) {
         model.addAttribute("books", bookRepository.findAll());
         return "booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+        return "redirect:/booklist";
+    }
+
+    @GetMapping("/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "/addbook";
+    }
+
+    @PostMapping("/newbook")
+    public String postMethodName(Book book) {
+        bookRepository.save(book);
+        return "redirect:/booklist";
     }
 
 }
