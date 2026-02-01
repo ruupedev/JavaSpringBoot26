5d0709ae3714076b064e4d8cd3af75981605605c
Stiller <ruupe.dev@gmail.com>
2026-02-01 17:35:49 +0200

    Added JPA, H2 and H2console dependencies. Created @Entity from book class. Added command line runner to add data on start. Modified properties. H2 Enabled

--- bookstore/src/main/java/bookstore/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/bookstore/bookstore/BookstoreApplication.java
@@ -1,13 +1,26 @@
 package bookstore.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import bookstore.bookstore.domain.Book;
+import bookstore.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository repository) {
+		return (args) -> {
+			repository.save(new Book("A Farewell to Arms", "Ernest Hemingway", 1929, "1232323-21", 12.00));
+			repository.save(new Book("Animal Farm", "George Orwell", 1945, "2212343-5", 9.00));
+		};
 	}
 
 }
--- bookstore/src/main/java/bookstore/bookstore/domain/Book.java
+++ bookstore/src/main/java/bookstore/bookstore/domain/Book.java
@@ -1,10 +1,33 @@
 package bookstore.bookstore.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String title;
     private String author;
     private Integer publicationYear;
     private String isbn;
-    private Float Double;
+    private Double price;
+
+    public Book() {
+
+    }
+
+    public Book(String title, String author, Integer publicationYear, String isbn, Double price) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isbn = isbn;
+        this.price = price;
+    }
+
 }
