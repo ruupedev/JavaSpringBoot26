4383c719e5a6ef618f88738a2b724c2be97b87ed
Stiller <ruupe.dev@gmail.com>
2026-02-01 18:31:15 +0200

    Listpage and GET request to display booklist

--- bookstore/src/main/java/bookstore/bookstore/domain/Book.java
+++ bookstore/src/main/java/bookstore/bookstore/domain/Book.java
@@ -30,4 +30,58 @@ public class Book {
         this.price = price;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Integer getPublicationYear() {
+        return publicationYear;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPublicationYear(Integer publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [id=" + id + ", title=" + title + ", author=" + author + ", publicationYear=" + publicationYear
+                + ", isbn=" + isbn + ", price=" + price + "]";
+    }
+
 }
--- bookstore/src/main/java/bookstore/bookstore/web/BookController.java
+++ bookstore/src/main/java/bookstore/bookstore/web/BookController.java
@@ -1,13 +1,28 @@
 package bookstore.bookstore.web;
 
+import bookstore.bookstore.domain.BookRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BookController {
 
+    private final BookRepository bookRepository;
+
+    BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     @GetMapping("/index")
     public String getIndex() {
         return "index";
     }
+
+    @GetMapping("/booklist")
+    public String getBooklist(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist";
+    }
+
 }
